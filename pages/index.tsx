2ad2837018ef86bfab25d95a5564620f07261433
welinhong <dmdanbi@gmail.com>
2023-02-04 15:04:09 +0900

    Remove redundant code

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,7 +78,6 @@ export default function Home() {
   return (
     <StyledContainer>
       <CustomHeader />
-      <AdBanner />
       <StyledContainerInner>
         {!isAnalysis && (
           <>
