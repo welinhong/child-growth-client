fe96805e1a51a194d294cd41ecfbb025dd1d770c
welinhong <dmdanbi@gmail.com>
2023-02-04 15:00:36 +0900

    formatting

--- src/components/AdBanner/index.tsx
+++ src/components/AdBanner/index.tsx
@@ -11,7 +11,9 @@ const AdBanner = () => {
         scrolling="no"
         referrerPolicy="unsafe-url"
       />
-      <Description>이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.</Description>
+      <Description>
+        이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.
+      </Description>
     </Container>
   )
 }
